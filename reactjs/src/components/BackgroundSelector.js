85d4924d01808e6326878df3a9c5c71750f558e8
Mariana Hans <marianahansv@gmail.com>
2025-03-10 20:18:29 -0600

    organized components into different files :)

---
+++ reactjs/src/components/BackgroundSelector.js
@@ -0,0 +1,10 @@
+function BackgroundSelector() {
+    return (
+      <div>
+        <h1>Background Selector</h1>
+        <button>Red</button>
+        <button>Green</button>
+        <button>Blue</button>
+      </div>
+    );
+  }
---
+++ reactjs/src/components/MusicPlayer.js
@@ -0,0 +1,13 @@
+function MusicPlayer(){
+    const playMusic = () => {
+      console.log("Music is playing");
+    }
+    return (
+      <div>
+        <h1>Music Player</h1>
+        <button>Play</button>
+        <button>Pause</button>
+        <button>Stop</button>
+      </div>
+    );
+  }
