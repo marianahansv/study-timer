15f5c6f3f5a71a6ba9f9b0de2bf2b69b2ac8e5f2
Your Name <enh839@usask.ca>
2025-03-10 20:14:30 -0600

    added main components

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,9 +1,42 @@
 import './App.css';
 
+function Timer() {
+  return (
+    <div>
+      <h1>Time: {new Date().toLocaleTimeString()}</h1>
+    </div>
+  );
+}
+
+function BackgroundSelector() {
+  return (
+    <div>
+      <h1>Background Selector</h1>
+      <button>Red</button>
+      <button>Green</button>
+      <button>Blue</button>
+    </div>
+  );
+}
+
+function MusicPlayer(){
+  return (
+    <div>
+      <h1>Music Player</h1>
+      <button>Play</button>
+      <button>Pause</button>
+      <button>Stop</button>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hi!</h1>
+      <h1>Cozy Study Room</h1>
+      <Timer />
+      <BackgroundSelector />
+      <MusicPlayer />
     </div>
   );
 }
