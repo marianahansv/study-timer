c1d43ccf7db70d90522d95236634a440ac1460e8
Your Name <enh839@usask.ca>
2025-03-10 20:18:29 -0600

    organized components into different files :)

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,35 +1,5 @@
 import './App.css';
 
-function Timer() {
-  return (
-    <div>
-      <h1>Time: {new Date().toLocaleTimeString()}</h1>
-    </div>
-  );
-}
-
-function BackgroundSelector() {
-  return (
-    <div>
-      <h1>Background Selector</h1>
-      <button>Red</button>
-      <button>Green</button>
-      <button>Blue</button>
-    </div>
-  );
-}
-
-function MusicPlayer(){
-  return (
-    <div>
-      <h1>Music Player</h1>
-      <button>Play</button>
-      <button>Pause</button>
-      <button>Stop</button>
-    </div>
-  );
-}
-
 function App() {
   return (
     <div className="App">
---
+++ reactjs/src/components/Timer.js
@@ -0,0 +1,7 @@
+function Timer() {
+    return (
+      <div>
+        <h1>Time: {new Date().toLocaleTimeString()}</h1>
+      </div>
+    );
+  }
