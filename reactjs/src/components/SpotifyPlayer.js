362617d12ecc160ccebc2b122aca0112377d6693
Mariana Hans <marianahansv@gmail.com>
2025-03-10 22:34:49 -0600

    added playlist to webpage

--- reactjs/src/components/SpotifyPlayer.js
+++ reactjs/src/components/SpotifyPlayer.js
@@ -5,21 +5,25 @@ const playlistsIds ={
   "lo-fi" : "3WLDIcG4Cx2UOPy0rbFhQn?si=51daab58fa7045df" ,
   "dark academia core" : "3iqq84bJKB5OsLXsQx7720?si=3caf533820e94e03" ,
   "coffee shop" : "19xWKcfAvw1epnlsTqjfTV?si=17deadd0ac1542ee",
-  "girly pop!" : ,
-  "a"
+  "girly pop!" : "7xkmRjcU7adwvRRtdHCLGr?si=4b8e1e74a2c14b70"
 };
 
-
 function SpotifyPlayer(){
-    const playMusic = () => {
-      console.log("Music is playing");
-    }
+    const [playlist, setPlaylist] = useState("chill vibes");
+
     return (
       <div>
-        <h1>pick your playlist!</h1>
-        <button>Play</button>
-        <button>Pause</button>
-        <button>Stop</button>
+        <div>
+          <h1>pick your playlist!</h1>
+          <select onChange={(e) => setPlaylist(e.target.value)}>
+            {Object.keys(playlistsIds).map((name) => (
+              <option key={name} value={name}>{name}</option>
+            ))}
+          </select>
+        </div>
+      <iframe 
+          src={`https://open.spotify.com/embed/playlist/${playlistsIds[playlist]}`}
+          width="300" height="200" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       </div>
     );
   }
